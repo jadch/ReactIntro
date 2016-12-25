d7ab1f30661c3f9835420a07705780712e3eb6d0
Jad <jad.chalita@gmail.com>
2016-12-25 15:48:02 +0100

    Refactoring - creating a js file for MyTitle and bundling using webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,22 +1,15 @@
-var div = React.DOM.div
-var h1 = React.DOM.h1
+/* global React ReactDOM */
+  // Declaring the global Reacy variables in  a comment for Standard
 
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-      h1({style: {color: this.props.color}}, this.props.title)
-      )
-    )
-  }
-})
+var div = React.DOM.div
+var MyTitle = require('./MyTitle')
 
 var MyTitleFact = React.createFactory(MyTitle)
 
 var MyFirstComponent = (
   div(null,
-    MyTitleFact({title: 'Props are great!', color:'red'}),
-    MyTitleFact({title: 'Props everywhere.', color:'mediumaquamarine'}),
+    MyTitleFact({title: 'Props are great!', color: 'red'}),
+    MyTitleFact({title: 'Props everywhere.', color: 'mediumaquamarine'}),
     MyTitleFact({title: 'I <3 Props'})
   )
 )
