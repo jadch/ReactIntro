f7914058f7538d26359721c92b59f667bf3f6190
Jad <jad.chalita@gmail.com>
2017-01-02 23:17:19 +0100

    Configured Standard.js for react using Eslint + webpack watch preLoader

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ module.exports = {
     chunks: false
   },
   module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader:"eslint-loader",
+        exclude: /node_modules/
+      }
+    ],
+
     loaders: [
       {
         test: /\.jsx?$/,
