430882795719f71eb692490204525b3f6e6ba938
Jad <jad.chalita@gmail.com>
2017-01-10 22:31:51 +0100

    Introducing component children and index routes

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -5,14 +5,18 @@ const ReactRouter = require('react-router')
 const Router = ReactRouter.Router
 const Route = ReactRouter.Route
 const hashHistory = ReactRouter.hashHistory
+const IndexRoute = ReactRouter.IndexRoute
 
 const Landing = require('./Landing')
 const Search = require('./Search')
+const Layout = require('./Layout')
 
 const App = () => (
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/search' component={Search} />
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Landing} />
+      <Route path='/search' component={Search} />
+    </Route>
   </Router>
   )
 
