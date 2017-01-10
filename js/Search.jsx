5242b2085edf2b72083f6c39ed67f75c301f2aec
Jad <jad.chalita@gmail.com>
2017-01-10 23:10:13 +0100

    Introducing state

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,14 +2,30 @@ const React = require('react')
 const data = require('../public/data')
 const ShowCard = require('./ShowCard')
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <ShowCard show={show} key={show.imdbid} />
-      ))}
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is the default search term'
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>Vletfix</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+        </header>
+        <div className='shows'>
+          {data.shows.map((show) => (
+            <ShowCard show={show} key={show.imdbid} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
