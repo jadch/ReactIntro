271f5ea75dac6edbdb6edb781b9b04ce6a5df82f
Jad <jad.chalita@gmail.com>
2017-01-15 11:45:02 +0100

    Routing for the Details page + code refacturing (data now loaded in ClientApp instead of Search)

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -6,16 +6,19 @@ const Router = ReactRouter.Router
 const Route = ReactRouter.Route
 const hashHistory = ReactRouter.hashHistory
 const IndexRoute = ReactRouter.IndexRoute
+const { shows } = require('../public/data')
 
 const Landing = require('./Landing')
 const Search = require('./Search')
 const Layout = require('./Layout')
+const Details = require('./Details')
 
 const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Layout}>
       <IndexRoute component={Landing} />
-      <Route path='/search' component={Search} />
+      <Route path='/search' component={Search} shows={shows} />
+      <Route path='/details/:id' component={Details} />
     </Route>
   </Router>
   )
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,12 +1,15 @@
 const React = require('react')
-const data = require('../public/data')
 const ShowCard = require('./ShowCard')
+const { object } = React.PropTypes
 
 const Search = React.createClass({
   getInitialState () {
     return {
       searchTerm: ''
     }
+  },
+  propTypes: {
+    route: object
   },
   handleSearchTermEvent (event) {
     this.setState({ searchTerm: event.target.value })
@@ -19,10 +22,8 @@ const Search = React.createClass({
           <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
         </header>
         <div className='shows'>
-          {data.shows
-            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
-            .map((show) => (
-              <ShowCard show={show} key={show.imdbid} />
+          {this.props.route.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0).map((show) => (
+            <ShowCard show={show} key={show.imdbid} />
           ))}
         </div>
       </div>
