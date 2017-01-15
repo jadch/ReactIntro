4cdb08ea9ff3c26ddadd9e26ee1c9bfdd4f246ae
Jad <jad.chalita@gmail.com>
2017-01-15 17:50:51 +0100

    Passing down show data to the Details page

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -13,14 +13,28 @@ const Search = require('./Search')
 const Layout = require('./Layout')
 const Details = require('./Details')
 
-const App = () => (
-  <Router history={hashHistory}>
-    <Route path='/' component={Layout}>
-      <IndexRoute component={Landing} />
-      <Route path='/search' component={Search} shows={shows} />
-      <Route path='/details/:id' component={Details} />
-    </Route>
-  </Router>
-  )
+const App = React.createClass({
+  assignShow (nextState, replace) {
+    const showArray = shows.filter((show) => show.imdbID === nextState.params.id)
+
+    if (showArray.length < 1) {
+      return replace('/')
+    }
+
+    Object.assign(nextState.params, showArray[0])
+    return nextState
+  },
+  render () {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Layout}>
+          <IndexRoute component={Landing} />
+          <Route path='/search' component={Search} shows={shows} />
+          <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
+        </Route>
+      </Router>
+    )
+  }
+})
 
 ReactDOM.render(<App />, document.getElementById('app'))
