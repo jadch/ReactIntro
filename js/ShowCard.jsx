8c17bdb17ff27c49a2c4a88ad806c2d93f959c9b
Jad <jad.chalita@gmail.com>
2017-01-19 13:04:07 +0100

    Linking ShowCard to the Details page

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,18 +1,22 @@
 const React = require('react')
+const { Link } = require('react-router')
 
 const ShowCard = (props) => (
-  <div className='show-card'>
-    <img src={`public/img/posters/${props.show.poster}`} className='show-card-img' />
-    <div className='show-card-text'>
-      <h3 className='show-card-title'>{props.show.title}</h3>
-      <h4 className='show-card-year'>({props.show.year})</h4>
-      <p className='show-card-description'>{props.show.description}</p>
+  <Link to={`/details/${props.show.imdbID}`}>
+    <div className='show-card'>
+      <img src={`public/img/posters/${props.show.poster}`} className='show-card-img' />
+      <div className='show-card-text'>
+        <h3 className='show-card-title'>{props.show.title}</h3>
+        <h4 className='show-card-year'>({props.show.year})</h4>
+        <p className='show-card-description'>{props.show.description}</p>
+      </div>
     </div>
-  </div>
+  </Link>
 )
 
 ShowCard.propTypes = {
-  show: React.PropTypes.object
+  show: React.PropTypes.object,
+  imdbID: React.PropTypes.string
 }
 
 module.exports = ShowCard
